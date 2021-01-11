79049e2ab81e85fd3ead2439de87320e053e6e17
4B6555S <monga.wakowa@gmail.com>
2021-01-11 22:02:14 +0200

    Update Connect.java

--- bpay-api/src/main/java/com/b6555s/bpay_api/network/Connect.java
+++ bpay-api/src/main/java/com/b6555s/bpay_api/network/Connect.java
@@ -1,5 +1,7 @@
 package com.b6555s.bpay_api.network;
 
 public class Connect {
-    public  String getDomain(){return "http://cloudbpay.bvortex.com/index.php/Api/process_payment";}
+    public  String getDomain(){
+        //return "http://cloudbpay.bvortex.com/index.php/Api/process_payment";
+        return "http://192.168.43.35/bpay/index.php/Api/process_payment";}
 }
