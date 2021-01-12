33b8b03d8a9778c1c942bd23c6e01da3fd1fdd9a
4B6555S <monga.wakowa@gmail.com>
2021-01-12 20:16:30 +0200

    add ssl

--- bpay-api/src/main/java/com/b6555s/bpay_api/network/Connect.java
+++ bpay-api/src/main/java/com/b6555s/bpay_api/network/Connect.java
@@ -2,6 +2,6 @@ package com.b6555s.bpay_api.network;
 
 public class Connect {
     public  String getDomain(){
-        return "http://cloudbpay.bvortex.com/index.php/Api/process_payment";
+        return "https://cloudbpay.bvortex.com/index.php/Api/process_payment";
     }
 }
